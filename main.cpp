981dbff4f3d3f16208a8c25c49fa82592df4b7a7
pchelka <avv363.aristov@yandex.ru>
2022-12-11 22:48:04 +0300

    finished everything till the 7th point inclusive

--- main.cpp
+++ main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <initializer_list>
 #include <vector>
+using std::cout, std::endl;
 
 template<typename T = double>
 class Matrix {
 private:
     int rows_ = 1;
     int cols_ = 1;
-    T* point;
 public:
-    Matrix(int rows) : rows_(rows) {
-        point = new T[rows_];
-        for (int i = 0; i < rows_; i++) {
-            *(point + i * rows_) = 0;
-        }
-    }
-    Matrix(int rows, int cols) : rows_(rows), cols_(cols) {
+    T* point = nullptr;
+    Matrix(int rows = 1, int cols = 1) : rows_(rows), cols_(cols) {
         point = new T[rows_ * cols_];
         for (int i = 0; i < rows_; i++) {
             for (int j = 0; j < cols_; j++) {
@@ -24,7 +19,6 @@ public:
         }
     }
 
-//    template<typename U>
     Matrix(std::initializer_list<T> lst) {
         cols_ = lst.size();
         point = new T[cols_];
@@ -34,7 +28,7 @@ public:
             i++;
         }
     }
-//    template<typename U>
+
     Matrix(std::initializer_list<std::initializer_list<T>> lst) {
         rows_ = lst.size();
         cols_ = (lst.begin())->size();
@@ -55,52 +49,133 @@ public:
     Matrix(const Matrix<T>& m) {
         this->rows_ = m.rows_;
         this->cols_ = m.cols_;
-        this->point = m.point;
-    }
-
-    Matrix<T>& operator=(Matrix<T> rhs) {
-        this->rows_ = rhs.rows_;
-        this->cols_ = rhs.cols_;
-        for (int i = 0; i < this->rows_; i++) {
-            for (int j = 0; j < this->cols_; j++) {
-                this->point + i * rows_ + j = rhs.point + + i * rows_ + j;
+        point = new T[rows_ * cols_];
+        for (int i = 0; i < rows_; i++) {
+            for (int j = 0; j < cols_; j++) {
+                *(point + i * rows_ + j) = *(m.point + i * rows_ + j);
             }
         }
+    }
+
+    Matrix( Matrix<T>&& rhs) {
+        this->rows_ = rhs.rows_;
+        this->cols_ = rhs.cols_;
+        delete[] point;
+
+        this->point = rhs.point;
+
+        rhs.point = nullptr;
+        rhs.rows_ = 0;
+        rhs.cols_ = 0;
+//        return *this;
+    }
+
+    Matrix<T>& operator=(const Matrix<T>& rhs) {
+        this->rows_ = rhs.rows_;
+        this->cols_ = rhs.cols_;
+        delete[] point;
+        point = new T[rows_ * cols_];
+        for (int i = 0; i < rows_; i++) {
+            for (int j = 0; j < cols_; j++) {
+                *(point + i * rows_ + j) = *(rhs.point + i * rows_ + j);
+            }
+        }
+        return *this;
+    }
+
+    Matrix<T>& operator=(Matrix<T>&& rhs)  noexcept{
+        if (this == &rhs) {
+            return *this;
+        }
+        this->rows_ = rhs.rows_;
+        this->cols_ = rhs.cols_;
+        delete[] point;
+
+        this->point = rhs.point;
+
+        rhs.point = nullptr;
+        rhs.rows_ = 0;
+        rhs.cols_ = 0;
+        return *this;
     }
 
     ~Matrix() {delete[] point;}
 
     friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& m) {
-        for (int i = 0; i < m.rows_; i++) {
-            for (int j = 0; j < m.cols_; j++) {
-                os << *(m.point + i * m.rows_ + j) << " ";
+        if (m.point == nullptr) {
+            os << "null matrix" << endl;
+        } else {
+            for (int i = 0; i < m.rows_; i++) {
+                for (int j = 0; j < m.cols_; j++) {
+                    os << *(m.point + i * m.rows_ + j) << " ";
+                }
+                os << "\n";
             }
-            os << "\n";
         }
         return os;
     }
+
+    T& operator()(int i, int j) {
+        if (i >= rows_ || j >= cols_) {
+            throw std::invalid_argument("index out of range\n");
+        }
+        return point[i * rows_ + j];
+    }
+    const T& operator()(int i, int j) const {
+        if (i >= rows_ || j >= cols_) {
+            throw std::invalid_argument("index out of range\n");
+        }
+        return point[i * rows_ + j];
+    }
+
+    friend Matrix<T> operator+(Matrix<T> lhs, const Matrix<T>& rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+
+    friend Matrix<T> operator-(Matrix<T> lhs, const Matrix<T>& rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+
+    friend Matrix<T> operator*(Matrix<T> lhs, const Matrix<T>& rhs) {
+        if (lhs.rows_ != rhs.cols_) {
+            throw std::invalid_argument("someone didn't learn linear algebra");
+        }
+
+        Matrix<T> tmp(lhs.rows_, rhs.cols_);
+        for (int i = 0; i < lhs.rows_; i++) {
+            for (int j = 0; j < rhs.cols_; j++) {
+                
+            }
+        }
+    }
+
+
 };
 
 int main() {
 
-    Matrix<int> m1(4);
-    std::cout << m1 << "\n";
-
-    Matrix<double> m2(4, 6);
-    std::cout << m2 << "\n";
+//    Matrix<int> m1(4);
+//    std::cout << m1 << endl;
+//
+//    Matrix<double> m2(4, 6);
+//    std::cout << m2 << endl;
 //
     Matrix<int> m5 = { {1, 2, 3}, {4, 5, 6} };
-    std::cout << m5 << "\n";
-
-    Matrix<int> m6 = { {1, 2, 3, 4, 5, 6} };
-    std::cout << m6 << "\n";
-
-    Matrix<int> m7 = {1, 2, 3, 4, 5, 6};
-    std::cout << m7 << "\n";
-
-//    Matrix<int> m8 = { {1}, {2}, {3}, {4}, {5}, {6} };
+//    std::cout << m5 << endl;
+//
+//    Matrix<int> m6 = { {1, 2, 3, 4, 5, 6} };
+//    std::cout << m6 << "\n";
+//
+//    Matrix<int> m7 = {1, 2, 3, 4, 5, 6};
+//    std::cout << m7 << "\n";
+//
+//    std::initializer_list<std::initializer_list<int>> list = { {1}, {2}, {3}, {4}, {5}, {6} };
+//    Matrix<int> m8 = list;
 //    std::cout << m8 << std::endl;
 
+    Matrix<int> m9(std::move(m5));
+    cout << m9 << endl;
+    cout << m5;
 }
-
-
